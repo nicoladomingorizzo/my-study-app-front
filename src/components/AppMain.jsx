53987c4d9928a9d465baf37330f82f936f3675d0
nicoladomingorizzo <nicoladomingorizzo@gmail.com>
2025-08-10 12:05:34 +0200

    implemented button delete

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,18 +1,18 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 
 export default function AppMain() {
 
-    const [tasks, setTasks] = useState([])
-    const [title, setTitle] = useState('')
-    const [description, setDescription] = useState('')
-    const [dueDate, setDueDate] = useState('')
+    const [tasks, setTasks] = useState([]);
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [dueDate, setDueDate] = useState('');
+    const apiUrl = `http://127.0.0.1:3030/api/tasks`;
 
-    const apiUrl = `http://127.0.0.1:3030/api/tasks`
     useEffect(() => {
         fetch(apiUrl)
             .then(res => res.json())
             .then(data => {
-                setTasks(data)
+                setTasks(data);
             })
     }, []);
 
@@ -30,7 +30,7 @@ export default function AppMain() {
     function handleSubmitTask(e) {
         e.preventDefault();
 
-        if (!name || !description || !dueDate) {
+        if (!title || !description || !dueDate) {
             alert('Per favore compila tutti i campi');
             return;
         }
@@ -45,19 +45,39 @@ export default function AppMain() {
             .then(res => res.json())
             .then(newTask => {
                 setTasks(prevTasks => [...prevTasks, newTask]);
+                setTitle('');
+                setDescription('');
+                setDueDate('');
             })
             .catch(error => console.error('Errore:', error));
     }
+
+    function handleRemoveClick(id) {
+        fetch(`http://127.0.0.1:3030/api/tasks/${id}`, {
+            method: 'DELETE'
+        })
+            .then(res => {
+                if (res.status === 204) {
+                    // Rimuovi dal DOM solo se il server ha eliminato
+                    setTasks(prev => prev.filter(task => task.id !== id));
+                } else {
+                    alert('Errore durante l\'eliminazione');
+                }
+            })
+            .catch(err => console.error('Errore DELETE:', err));
+    }
+
+
     return (
         <>
             <div className="container py-4">
-                <div className="row row-cols-1 row-cols-md-2">
+                <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3">
                     {tasks.map(task => {
                         return (
-                            <div className="col">
-                                <div className="card mx-auto my-2 p-3" key={task.id} style={{ maxWidth: '900px' }}>
+                            <div className="col" key={task.id}>
+                                <div className="card mx-auto my-2 p-3" style={{ maxWidth: '900px' }}>
                                     <div className="d-flex justify-content-between align-items-center mb-2">
-                                        <p className="m-0"><strong>Task:</strong> {task.title}</p>
+                                        <p className="m-0"><strong>Task:</strong>{task.title}</p>
                                         <button className="btn btn-outline-warning btn-sm">
                                             <i className="bi bi-pencil-square"></i>
                                         </button>
@@ -77,7 +97,7 @@ export default function AppMain() {
                                         </button>
                                     </div>
                                     <div className="text-end">
-                                        <button className="btn btn-outline-danger btn-sm">
+                                        <button className="btn btn-outline-danger btn-sm" onClick={() => handleRemoveClick(task.id)}>
                                             <i className="bi bi-trash"></i>
                                         </button>
                                     </div>
