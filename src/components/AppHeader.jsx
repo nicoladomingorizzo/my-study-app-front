93d56bcd613f1bdbf0dfa798e0de59814cadeffd
nicoladomingorizzo <nicoladomingorizzo@gmail.com>
2025-08-20 12:05:21 +0200

    removed delete all button

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -5,17 +5,13 @@ export default function AppHeader({ editingTaskId, title, setTitle, description,
         <div className="bg-body-tertiary py-5">
             <div className="container text-center justify-content-around align-items-center d-flex ">
                 <div className="Title">
-                    <h1 className="mb-4">Tasks List</h1>
+                    <h1>Tasks List</h1>
                     <p>by Nicola Rizzo</p>
                 </div>
 
                 {/* TaskForm integrato nell'header */}
-                <div className="mt-2 buttons">
+                <div className="mt-2 taskForm">
                     <TaskForm editingTaskId={editingTaskId} title={title} setTitle={setTitle} description={description} setDescription={setDescription} dueDate={dueDate} setDueDate={setDueDate} handleSubmitCreate={handleSubmitCreate} handleSubmitUpdate={handleSubmitUpdate} handleEraseUpdateTask={handleEraseUpdateTask} />
-                    <button className="btn btn-outline-danger">
-                        <i class="bi bi-trash"></i>
-                        Elimina tutto
-                    </button>
                 </div>
             </div>
         </div>
