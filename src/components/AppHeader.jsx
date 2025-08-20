98b777163cf035f4c0e08c6ae20ddaa2581315b0
nicoladomingorizzo <nicoladomingorizzo@gmail.com>
2025-08-20 11:29:34 +0200

    added justify content and align items

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -3,7 +3,7 @@ import TaskForm from "./TaskForm";
 export default function AppHeader({ editingTaskId, title, setTitle, description, setDescription, dueDate, setDueDate, handleSubmitCreate, handleSubmitUpdate, handleEraseUpdateTask }) {
     return (
         <div className="bg-body-tertiary py-5">
-            <div className="container text-center d-flex ">
+            <div className="container text-center justify-content-around align-items-center d-flex ">
                 <div className="Title">
                     <h1>Tasks List</h1>
                     <p>by Nicola Rizzo</p>
